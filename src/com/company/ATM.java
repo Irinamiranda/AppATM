eace982403ce5e938a2aacc4b4189da1a2e40fba
GBTC440003UR <GBTC440003UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-10-03 13:55:10 -0400

    completed

--- src/com/company/ATM.java
+++ src/com/company/ATM.java
@@ -1,41 +1,31 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ATM {
 
     public static void run() {
+        //create ArrayList to keep all accounts
+        ArrayList<Account> accounts = new ArrayList<>();
         //create person object to keep user's information
         Account person = new Account("12345", "Irina", "1234", 500.0);
+        accounts.add(person);
+        Account person2 = new Account("123456", "Andrey", "123", 200.0);
+        accounts.add(person2);
+        Account person3 = new Account("1234", "Larissa", "12", 250.0);
+        accounts.add(person3);
         //create Scanner object that allows users to make an input
         Scanner s = new Scanner(System.in);
 
-/*
-        // ---- initial setup
-        System.out.println("Enter your account number");
-        String accountNumber = s.nextLine();
-        person.setAccountNumber(accountNumber);
-
-        System.out.println("Enter your PIN");
-        String pinCode = s.nextLine();
-        person.setPinCode(pinCode);
-
-        System.out.println("Enter your name");
-        String userName = s.nextLine();
-        person.setUserName(userName);
-
-        System.out.println("Enter your balance");
-        String balance = s.nextLine();
-        person.setBalance(Double.parseDouble(balance));
-        // ----
-*/
 
         System.out.println("Starting ATM routine..\n");
 
         for (int i = 0; i < 3; i++) {
-            if (verifyAccount(person)) {
-                System.out.println("Welcome to your account, " + person.getUserName());// write your code here
-                System.out.println("Your balance is: " + person.getBalance());
+            Account acct = selectAccount(accounts);
+            if (acct != null) {
+                System.out.println("Welcome to your account, " + acct.getUserName());// write your code here
+                System.out.println("Your balance is: " + acct.getBalance());
                 //prompt the user to pick procedure
                 System.out.println("Do you wish deposit or withdraw?");
                 String choice = " ";
@@ -45,18 +35,18 @@ public class ATM {
                 if (answer.equalsIgnoreCase("deposit")) {
                     System.out.println("What amount are you going to add to your account?");
                     double deposit = s.nextDouble();
-                    double totalBalance = person.getBalance() + deposit;
-                    person.setBalance(totalBalance);
-                    System.out.println("Your balance now is: " + person.getBalance());
+                    double totalBalance = acct.getBalance() + deposit;
+                    acct.setBalance(totalBalance);
+                    System.out.println("Your balance now is: " + acct.getBalance());
                 }
                 //if user picks to withdraw
                 else if (answer.equalsIgnoreCase("withdraw")) {
 
                     System.out.println("What amount do you wish to withdraw?");
                     double withdraw = s.nextDouble();
-                    double totalBalance = person.getBalance() - withdraw;
-                    person.setBalance(totalBalance);
-                    System.out.println("Your balance now is: " + person.getBalance());
+                    double totalBalance = acct.getBalance() - withdraw;
+                    acct.setBalance(totalBalance);
+                    System.out.println("Your balance now is: " + acct.getBalance());
                 } //if user input wrong answer
                 else {
                     System.out.println("Wrong choice! Try again!");
@@ -71,7 +61,9 @@ public class ATM {
         System.out.println("Your account is locked");
     }
 
-    public static boolean verifyAccount(Account account) {
+    // create a method to search the match
+    public static Account selectAccount(ArrayList<Account> accounts) {
+
         Scanner s = new Scanner(System.in);
 
         System.out.println("Enter your account number");
@@ -79,8 +71,15 @@ public class ATM {
         System.out.println("Enter your PIN code");
         String pin = s.nextLine();
 
-        return account.getAccountNumber().equals(acc) && account.getPinCode().equals(pin);
-    }
+        for (Account acct : accounts) {
+            if (acct.getAccountNumber().equals(acc) && acct.getPinCode().equals(pin)) {
+                return acct;
+            }
+        }
+
+        return null;
+
+        }
 
     public static void main(String[] args) {
         ATM.run();
